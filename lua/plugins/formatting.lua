81f4e45094bd1f0ef13822e312e2a7f130acacb7
Konkov Nikita <n.konkov@rqc.ru>
2026-01-25 21:38:48 +0300

    formatting works of java files by prettier finally works

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,32 +2,39 @@
 
 -- Formatting: conform.nvim configuration
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>cf",
-        function()
-          require("conform").format({ lsp_fallback = true })
-        end,
-        mode = { "n", "v" },
-        desc = "Format",
-      },
-    },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        java = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    },
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>cf",
+				function()
+					require("conform").format({ lsp_fallback = true })
+				end,
+				mode = { "n", "v" },
+				desc = "Format",
+			},
+		},
+		opts = {
+			formatters = {
+				prettier_java = {
+					command = "prettier",
+					args = { "--stdin-filepath", "$FILENAME", "--plugin", "prettier-plugin-java" },
+					stdin = true,
+				},
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				java = { "prettier_java" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		},
+	},
 }
