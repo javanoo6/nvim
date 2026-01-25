6291e5af1daf7da97195ae6f9259a8315e3ac24b
Konkov Nikita <n.konkov@rqc.ru>
2026-01-25 19:07:43 +0300

    sucks and no keymaps rearranged

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,84 +3,78 @@
 local util = require("util")
 local map = util.map
 
--- Better up/down
+-- Movement (no leader)
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
--- Window navigation
+-- Windows
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
--- Resize windows
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase width" })
 
 -- Move lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move lines down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move lines up" })
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Buffers
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
-map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to other buffer" })
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Alternate buffer" })
 
--- Clear search with <esc>
-map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+-- Quickfix/Loclist
+map("n", "[q", "<cmd>cprev<cr>", { desc = "Prev quickfix" })
+map("n", "]q", "<cmd>cnext<cr>", { desc = "Next quickfix" })
+map("n", "[l", "<cmd>lprev<cr>", { desc = "Prev loclist" })
+map("n", "]l", "<cmd>lnext<cr>", { desc = "Next loclist" })
 
--- Save file
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+-- Diagnostics
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+map("n", "<leader>xd", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+map("n", "<leader>xl", vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
 
--- Better indenting
+-- Editing primitives
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear hlsearch" })
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save" })
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- New file
+-- Files
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
 
 -- Quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- Lazy
-map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>pl", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- Splits
 map("n", "<leader>-", "<C-W>s", { desc = "Split below" })
 map("n", "<leader>|", "<C-W>v", { desc = "Split right" })
 
 -- Tabs
-map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last tab" })
-map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First tab" })
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New tab" })
-map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next tab" })
-map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close tab" })
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Prev" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close" })
 
--- Toggle options
-map("n", "<leader>us", function() util.toggle("spell") end, { desc = "Toggle spelling" })
-map("n", "<leader>uw", function() util.toggle("wrap") end, { desc = "Toggle wrap" })
-map("n", "<leader>ul", function() util.toggle("relativenumber") end, { desc = "Toggle relative line numbers" })
-map("n", "<leader>un", function() util.toggle("number") end, { desc = "Toggle line numbers" })
+-- UI Toggles
+map("n", "<leader>us", function() util.toggle("spell") end, { desc = "Spelling" })
+map("n", "<leader>uw", function() util.toggle("wrap") end, { desc = "Wrap" })
+map("n", "<leader>ul", function() util.toggle("relativenumber") end, { desc = "Relative numbers" })
+map("n", "<leader>un", function() util.toggle("number") end, { desc = "Line numbers" })
 
--- Diagnostics
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-map("n", "<leader>xd", vim.diagnostic.open_float, { desc = "Show diagnostic" })
-map("n", "<leader>xl", vim.diagnostic.setloclist, { desc = "Diagnostic loclist" })
-
--- Quickfix navigation
-map("n", "[q", "<cmd>cprev<cr>", { desc = "Prev quickfix" })
-map("n", "]q", "<cmd>cnext<cr>", { desc = "Next quickfix" })
-
--- Format
+-- Code
 map({ "n", "v" }, "<leader>cf", function() util.format() end, { desc = "Format" })
