0518970fd250a4fab2899834521a8493989d03a8
Konkov Nikita <n.konkov@rqc.ru>
2026-01-25 20:15:46 +0300

    treesitter refactoring

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,36 +2,37 @@
 
 -- Treesitter: Syntax highlighting and code understanding
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "html",
-          "java",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "vim",
-          "vimdoc",
-          "yaml",
-          "xml",
-          "groovy",
-          "kotlin",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"vim",
+				"vimdoc",
+				"yaml",
+				"xml",
+				"groovy",
+				"kotlin",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
