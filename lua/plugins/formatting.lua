de86195d8be9fd02807cc50a209903cff8c15b3a
Konkov Nikita <n.konkov@rqc.ru>
2026-01-26 10:59:08 +0300

    formatting is now togglable

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -5,7 +5,7 @@ return {
 	{
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
+		cmd = { "ConformInfo", "FormatEnable", "FormatDisable" },
 		keys = {
 			{
 				"<leader>cf",
@@ -31,10 +31,28 @@ return {
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_fallback = true }
+			end,
 		},
+		config = function(_, opts)
+			require("conform").setup(opts)
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, { bang = true, desc = "Disable format on save" })
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, { desc = "Enable format on save" })
+		end,
 	},
 }
