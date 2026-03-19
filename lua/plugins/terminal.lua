1324946845267257d675325558dd5f300ec5e619
Konkov Nikita <n.konkov@rqc.ru>
2026-03-19 20:37:41 +0300

    toggle term addition

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,20 @@
+-- toggleterm.nvim - persistent togglable terminal
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	keys = {
+		{ "<A-a>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal", mode = { "n", "t" } },
+	},
+	opts = {
+		open_mapping = false, -- managed via keys above
+		hide_numbers = true,
+		direction = "float",
+		float_opts = {
+			border = "rounded",
+		},
+		-- <A-c> to hide from within terminal mode
+		on_create = function(term)
+			vim.keymap.set("t", "<A-c>", function() term:toggle() end, { buffer = term.bufnr, desc = "Hide terminal" })
+		end,
+	},
+}
