0fdc85d5fac99d197f2b009edf6ccd8585d2cf28
Konkov Nikita <n.konkov@rqc.ru>
2026-03-20 21:01:18 +0300

    golangcli-lint fix

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -14,6 +14,14 @@ return {
 				python = { "ruff" },
 			}
 
+			-- Fix: golangci-lint needs cwd set to the file's directory (or module root)
+			-- so it can find go.mod, otherwise it exits with code 5 (no Go files found)
+			lint.linters.golangcilint = vim.tbl_extend("force", lint.linters.golangcilint, {
+				cwd = function()
+					return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+				end,
+			})
+
 			-- Run linting on save
 			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
 				group = require("util").augroup("lint"),
