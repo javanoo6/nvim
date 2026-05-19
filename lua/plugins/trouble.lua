cdcc1be7cabc78e164d76f7857c766c6827ca32b
Konkov Nikita <n.konkov@rqc.ru>
2026-05-19 14:12:10 +0300

    a bit more fixes of lsp usage|refs

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -9,7 +9,8 @@ return {
     { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
     { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
     { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols" },
-    { "<leader>cl", "<cmd>Trouble lsp toggle<cr>", desc = "LSP Definitions/references" },
+    { "<leader>cl", "<cmd>Trouble lsp_calls toggle<cr>", desc = "Call hierarchy" },
+    { "<leader>cu", "<cmd>Trouble lsp_usages toggle<cr>", desc = "Usages / references" },
     { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
     { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List" },
     {
@@ -27,5 +28,53 @@ return {
       desc = "Prev trouble",
     },
   },
-  opts = {},
+  opts = function()
+    local function is_source_backed_location(item)
+      local filename = item.filename or ""
+      if filename == "" then
+        return true
+      end
+
+      return not filename:match("^jdt://")
+        and not filename:match("^jar:file://")
+        and not filename:match("^zipfile://")
+        and not filename:match("%.class$")
+    end
+
+    return {
+      filters = {
+        source_only = function(item, value)
+          return not value or is_source_backed_location(item)
+        end,
+      },
+      modes = {
+        lsp_calls = {
+          desc = "LSP incoming and outgoing calls",
+          sections = {
+            "lsp_incoming_calls",
+            "lsp_outgoing_calls",
+          },
+          focus = false,
+          win = { position = "right" },
+          preview = { type = "main", scratch = true },
+          filter = {
+            source_only = true,
+          },
+        },
+        lsp_usages = {
+          mode = "lsp_references",
+          desc = "LSP usages and references",
+          focus = false,
+          win = { position = "right" },
+          preview = { type = "main", scratch = true },
+          params = {
+            include_declaration = false,
+          },
+          filter = {
+            source_only = true,
+          },
+        },
+      },
+    }
+  end,
 }
